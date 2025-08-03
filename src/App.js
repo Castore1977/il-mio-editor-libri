a9115ec49b8c33a3cdd406db92f54f27e1774dd7
Castore1977 <enrico.bastelli@gmail.com>
2025-08-03 21:48:31 +0200

    Font corretti v9

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { getFirestore, collection, doc, getDocs, setDoc, deleteDoc, onSnapshot, query } from "firebase/firestore";
 
-
 // --- FIREBASE CONFIGURATION ---
 // ATTENZIONE: Queste chiavi sono di esempio e non funzioneranno.
 // Sostituiscile con le tue chiavi Firebase reali.
